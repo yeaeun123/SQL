f022573931d17abe92ad4ac306bca64023a45919
yeaeun123 <yeaeun0125@gmail.com>
2024-04-26 16:37:16 +0900

    20240426 오후

--- 02-DML-JOIN-AGGREGATION.sql
+++ 02-DML-JOIN-AGGREGATION.sql
@@ -42,3 +42,117 @@ SELECT emp.first_name,
     dept.department_name
 FROM employees emp JOIN departments dept USING (department_id);
 
+---------------------
+-- Theta Join (세타조인)
+---------------------
+
+-- Join 조건이 ( = )가 아닌 다른 조건들
+
+-- 급여가 직군 평균 급여보다 낮은 직원들의 목록
+SELECT  
+    emp.employee_id,
+    emp.first_name,
+    emp.salary,
+    emp.job_id,
+    j.job_id,
+    j.job_title
+FROM
+    employees emp
+    JOIN jobs j
+        ON emp.job_id = j.job_id
+WHERE emp.salary <= (j.min_salary + j.max_salary ) / 2;
+
+
+
+--------------------------
+--OUTER JOIN
+--------------------------
+-- 조건을 만족하는 짝이 없는  튜플도 NULL을 포함하여 결과 출력 참여시키는 방법
+-- 모든 결과를 표현한 테이블이 어느 쪽에 위치하는거에 따라 LEFT, RIGHT,FULL OUTER JOIN으로 구분
+-- ORACLE SQL의 경우 NULL이 출력되는 쪽에 (+)를 붙인다.
+
+-------------------
+--LEFT OUTER JOIN
+-------------------
+-- LEFT 테이블의 모든 레코드가 출력 결과에 참여
+-- Oracle SQL
+SELECT emp.first_name,
+    emp.department_id,
+    dept.department_id,
+    department_name
+FROM employees emp, departments dept
+WHERE emp.department_id = dept.department_id(+); 
+-- null이 포함된 테이블 쪽에 (+) 표기
+
+SELECT * FROM employees WHERE department_id IS NULL;    
+--Kimberly ->부서에 소속되지 않음
+
+
+-- ANSI SQL :명시적으로 JOIN 방법을 정한다.
+SELECT first_name,
+    emp.department_id,
+    dept.department_id,
+    department_name
+FROM employees emp 
+     LEFT OUTER JOIN departments dept
+        ON emp.department_id = dept.department_id;
+
+-------------------
+-- RIGHT OUTER JOIN
+-------------------
+-- RIGHT 테이블의 모든 레코드가 출력 결과에 참여
+
+--Oracle SQL
+SELECT first_name,
+    emp.department_id,
+    dept.department_id,
+    department_name
+FROM employees emp, departments dept
+WHERE emp.department_id (+) = dept.department_id; 
+-- departments 테이블 레코드 전부를 출력에 참여시키기 / 총 122 레코드 출력
+
+-- ANSI SQL(표준)
+SELECT first_name,
+    emp.department_id,
+    dept.department_id,
+    department_name
+FROM employees emp
+    RIGHT OUTER JOIN departments dept
+        ON emp.department_id = dept.department_id;
+-- null값을 포함해서 총 122 레코드 출력
+
+-------------------
+-- FULL OUTER JOIN
+-------------------
+-- JOIN에 참여한 모든 테이블의 모든 레코드를 출력에 참여시킨다.
+-- 짝이 없는 레코드들은 NULL을 포함해서 출력에 참여시킨다.
+-- Oracle엔 없고 ANSI SQL로만 실행 가능하다.
+
+-- ANSI SQL
+SELECT first_name,
+    emp.department_id,
+    dept.department_id,
+    department_name
+FROM employees emp 
+    FULL OUTER JOIN departments dept
+        ON emp.department_id = dept.department_id;
+-- null값을 포함해서 총 123 레코드 출력
+
+
+-------------------
+-- NATURAL JOIN
+-------------------
+-- 조인할 테이블에 같은 이름의 컬럼이 있을 경우, 해당 컬럼을 기준으로 JOIN
+-- 실제 본인이 JOIN할 조건과 일치하는지 확인 ! 
+
+SELECT * FROM employees emp NATURAL JOIN departments dept;
+
+--아래 두 줄과 같이 실수 할 여지가 많음 /조심해야함 
+--SELECT *  FROM employees emp JOIN departments dept ON emp.department_id = dept.department_id;
+--SELECT *  FROM employees emp JOIN departments dept ON emp.manager_id = dept.manager_id;
+
+SELECT *  FROM employees emp JOIN departments dept ON emp.manager_id = dept.manager_id AND emp.department_id = dept.department_id;
+
+-------------------
+-- SELF JOIN
+-------------------
