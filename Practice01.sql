857c295abb642511f53b401ba6cbbc5b1b16a61e
yeaeun123 <yeaeun0125@gmail.com>
2024-04-26 10:54:04 +0900

    Update Practice01.sql

--- Practice01.sql
+++ Practice01.sql
@@ -6,7 +6,8 @@
 --임부터 출력이 되도록 하세요. 이름(first_name last_name), 월급(salary), 전화번호
 --(phone_number), 입사일(hire_date) 순서이고 “이름”, “월급”, “전화번호”, “입사일” 로 컬럼이
 --름을 대체해 보세요.
-SELECT first_name 이름, salary 월급, phone_number 전화번호,hire_date 입사일
+SELECT first_name ||' '|| last_name 이름, salary 월급, 
+    phone_number 전화번호,hire_date 입사일
 FROM employees 
 ORDER BY hire_date ASC;
 
