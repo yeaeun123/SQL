ef27812fb9ce76864106f5306c37a2f3cfad98b9
yeaeun123 <yeaeun0125@gmail.com>
2024-04-29 17:47:32 +0900

    240429 오후

--- 02-DML-JOIN-AGGREGATION.sql
+++ 02-DML-JOIN-AGGREGATION.sql
@@ -240,8 +240,8 @@ FROM employees;
 SELECT department_id, AVG(salary)   --오류! 
 FROM employees;
 
-SELECT department_id FROM employees; -- 여러개의 레코드
-SELECT AVG(salary) FROM employees;  -- 단일 레코드
+SELECT department_id FROM employees; -- 여러개의 레코드(행)
+SELECT AVG(salary) FROM employees;  -- 단일 레코드(행)
 
 SELECT department_id, salary
 FROM employees
@@ -253,13 +253,117 @@ FROM employees
 GROUP BY department_id      -- 집계를 위해 특정 컬럼을 기준으로 그룹핑
 ORDER BY department_id;
 
+-- 부서별 평균 급여에 부서명도 함께 출력?
+SELECT emp.department_id, dept.department_name, ROUND(AVG(emp.salary), 2)
+FROM employees emp 
+    JOIN departments dept 
+        ON emp.department_id = dept.department_id
+GROUP BY emp.department_id, dept.department_name
+ORDER BY emp.department_id;
+
+-- GROUP BY절 이후에는 GROUP BY에 참여한 컬럼과 집계 함수만 남는다.
+
+-- 평균 급여가 7000달러 이상인 부서만 출력?
+SELECT department_id, AVG(salary)
+FROM employees
+WHERE AVG(salary) >= 7000       -- 아직 집계 함수 시행되지 않은 상태 -> 집계함수의 비교 불가
+GROUP BY department_id
+ORDER BY department_id;
+
+-- 집계 함수 이후의 조건 비교 HAVING 절을 이용
+SELECT department_id, AVG(salary)
+FROM employees
+GROUP BY department_id
+    HAVING AVG(salary) >= 7000  -- GROUP BY aggregation의 조건 필터링
+ORDER BY department_id;
+
+/*급여(salary) 합계가 20000 이상인 부서의 부서 번호와 인원 수, 급여 합계를 출력하기
+위해 다음과 같은 쿼리를 작성했다.*/
+SELECT department_id, COUNT(*), SUM(salary)
+FROM employees
+GROUP BY department_id
+    HAVING SUM(salary) > 20000;
+    
+/* 단일 SQP작성법
+1. 최종 출력될 정보에 따라 원하는 컬럼을 SELECT 절에 추가
+2. 원하는 정보를 가진 테이블들을 FROM 절에 추가
+3. WHERE 절에 알맞은 JOIN 조건 추가
+4. WHERE 절에 알맞은 검색 조건 추가(SELECTION)
+5. 필요에 따라 GROUP BY, HAVING 등을 통해 Grouping하고 Aggregate
+6. 정렬 조건 ORDER BY에 추가*/
 
 
+-- ROLLUP
+-- GROUP BY 절과 함께 사용
+-- 그룹지어진 결과에 대한 좀 더 상세한 요약을 제공하는 기능 수행
+-- 일종의 Item Total
+SELECT department_id, job_id, SUM(salary)
+FROM employees
+GROUP BY ROLLUP(department_id, job_id);
 
+-- CUBE
+-- GROUP BY 절과 함께 사용
+-- CrossTab에 대한 Summary를 함께 추출하는 함수
+-- ROLLUP 함수에 의해 출력되는 Item Total 값과 함께
+-- Column Total 값을 함께 추출
+SELECT department_id, job_id, SUM(salary)
+FROM employees
+GROUP BY CUBE(department_id, job_id)
+ORDER BY department_id;
 
+-------------------------
+-- SUBQUERY 
+-------------------------
+-- Single-Row Subquery
+-- Subquery의 결과가 한 ROW인 경우
+-- Single-Row Operator를 사용해야 함: =, >, >=, <, <=, <>
 
+-- 모든 직원 급여의 중앙값보다 많은 급여를 받는 사원? 
 
+-- 1) 직원 급여의 중앙값? 
+-- 2) 1)번의 결과보다 많은 급여를 받는 직원의 목록?
 
+-- 1) 직원 급여의 중앙값
+SELECT MEDIAN(salary) FROM employees;   -- 6200
+-- 2) 1)번의 결과(6200)보다 많은 급여를 받는 직원의 목록?
+SELECT first_name, salary
+FROM employees
+WHERE salary >= 6200;
+
+-- 1),2)쿼리 합치기
+SELECT first_name, salary
+FROM employees
+WHERE salary >= (SELECT MEDIAN(salary) FROM employees)
+ORDER BY salary DESC;
+
+-- Susan보다 늦게 입사한 사원의 정보?
+-- 1) Susan의 입사일
+-- 2) 1)번의 결과보다 늦게 입사한 사원의 정보 추출
+
+-- 1) Susan의 입사일
+SELECT hire_date FROM employees
+WHERE first_name = 'Susan';     --12/06/07
+
+-- 2) 1)번의 결과보다 늦게 입사한 직원 목록
+SELECT first_name, hire_date
+FROM employees
+WHERE hire_date > '12/06/07';
+
+-- 1),2) 쿼리 합치기
+SELECT first_name, hire_date
+FROM employees
+WHERE hire_date > (SELECT hire_date FROM employees WHERE first_name = 'Susan')
+ORDER BY hire_date;
+
+-- 연습문제
+-- 모든 직원 급여의 중앙 값보다 급여를 많이 받으면서 수잔보다 늦게 입사한 직원 목록?
+-- 조건1. 모든 직원 급여의 중앙 값보다 급여를 많이 받는다.
+-- 조건2. 수잔의 입사일보다 hire_date 늦게 입사한 직원
+SELECT first_name, salary, hire_date
+FROM employees
+WHERE salary >= (SELECT MEDIAN(salary) FROM employees) AND  -- 조건1
+    hire_date > (SELECT hire_date FROM employees WHERE first_name = 'Susan') --조건2
+ORDER BY hire_date ASC, salary DESC;
 
 
 
