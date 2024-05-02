18009b159e7fee3159e0180db835904e0b1f2ce8
yeaeun123 <yeaeun0125@gmail.com>
2024-05-02 14:48:56 +0900

    Update 03-oracle-dcl-and-ddl.sql

--- 03-oracle-dcl-and-ddl.sql
+++ 03-oracle-dcl-and-ddl.sql
@@ -210,9 +210,29 @@ SELECT CONSTRAINT_NAME, CONSTRAINT_TYPE,
 FROM USER_CONSTRAINTS
 WHERE TABLE_NAME = 'BOOK';
 
+--INSERT : 테이블에 새 레코드(튜플) 추가
+-- 제공된 컬럼 목록의 순서와 타입, 값 목록의 순서와 타입이 일치해야한다.
+-- 컬럼 목록을 제공하지 않으면 테이블 생성시 정의된 컬럼의 순서와 타입을 따른다.
 
+--삽입 : INSERT INTO 테이블명 VALUES (값의 목록);
+---> 값 목록의 순서 -> TABLE 설계대로 (전체)
 
+-- 삽입2: INSERT INTO 테이블명 (컬럼이름목록) VALUES (값목록);
+---> (컬럼 이름목록) 순서대로-> VALUES 값을 제공 (특정 컬럼만)
 
+-- 컬럼 목록이 제시되지 않았을 때
+INSERT INTO author
+VALUES (1, '박경리', '토지 작가');
+
+-- 컬럼 목록을 제시했을 때, 
+-- 제시한 컬럼의 순서와 타입대로 값 목록을 제공해야 한다.
+INSERT INTO author (author_id, author_name)
+VALUES(2, '김영하');
+
+-- 컬럼 목록을 제공했을 때,
+-- 테이블 생성시 정의된 컬럼의 순서와 상관없이 데이터 제공 가능
+INSERT INTO author (author_name, author_id, author_desc)
+VALUES ('류츠신', 3, '삼체 작가');
 
 
 
