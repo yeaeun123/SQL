3b3a42368e13018b34f5427dd55a42d89929eb79
yeaeun123 <yeaeun0125@gmail.com>
2024-05-02 16:44:12 +0900

    240502 오후

--- 03-oracle-dcl-and-ddl.sql
+++ 03-oracle-dcl-and-ddl.sql
@@ -210,6 +210,7 @@ SELECT CONSTRAINT_NAME, CONSTRAINT_TYPE,
 FROM USER_CONSTRAINTS
 WHERE TABLE_NAME = 'BOOK';
 
+
 --INSERT : 테이블에 새 레코드(튜플) 추가
 -- 제공된 컬럼 목록의 순서와 타입, 값 목록의 순서와 타입이 일치해야한다.
 -- 컬럼 목록을 제공하지 않으면 테이블 생성시 정의된 컬럼의 순서와 타입을 따른다.
@@ -224,17 +225,142 @@ WHERE TABLE_NAME = 'BOOK';
 INSERT INTO author
 VALUES (1, '박경리', '토지 작가');
 
+SELECT * FROM author;
+
 -- 컬럼 목록을 제시했을 때, 
 -- 제시한 컬럼의 순서와 타입대로 값 목록을 제공해야 한다.
 INSERT INTO author (author_id, author_name)
 VALUES(2, '김영하');
+
+SELECT * FROM author;
 
 -- 컬럼 목록을 제공했을 때,
 -- 테이블 생성시 정의된 컬럼의 순서와 상관없이 데이터 제공 가능
 INSERT INTO author (author_name, author_id, author_desc)
 VALUES ('류츠신', 3, '삼체 작가');
 
+SELECT * FROM author;
 
+--TRUNCATE TABLE 테이블명; ->전체 레코드 삭제 (속도빠름/완전삭제/복구불가)
+TRUNCATE TABLE author;
+
+ROLLBACK;   -- 반영 취소 (되돌리기)
+
+SELECT * FROM author;
+
+INSERT INTO author
+VALUES(1, '박경리', '토지 작가');
+INSERT INTO author (author_id, author_name)
+VALUES(2, '김영하');
+INSERT INTO author (author_name, author_id, author_desc)
+VALUES ('류츠신', 3, '삼체 작가');
+
+SELECT * FROM author;
+
+--ALTER TABLE author RENAME COLUMN ANTHOR_NAME TO author_name;
+COMMIT;     -- 변경사항 반영
+
+SELECT * FROM author;
+
+-- UPDATE
+-- 특정 레코드의 컬럼 값을 변경한다.
+-- WHERE 절이 없으면 모든 레코드가 변경!!! 
+-- 가급적 WHRER절로 변경하고자 하는 레코드를 지정하도록 한다.
+
+--변경1.: UPDATE 테이블명 SET 변경내용; -> 전체레코드 변경
+
+--변경2: UPDATE 테이블명 SET 변경내용 WHERE 변경할 레코드 조건; 
+        --->변경할 레코드 조건에 해당하는 레코드만 변경
+
+UPDATE author 
+SET author_desc = '알쓸신잡 출연';
+
+SELECT * FROM author;
+
+ROLLBACK;
+
+SELECT * FROM author;
+
+UPDATE author
+SET author_desc = '알쓸신잡 출연'
+WHERE author_name = '김영하';
+
+SELECT * FROM author;
+
+COMMIT;
+
+--연습
+-- hr.employees 테이블을 기반으로 department_id 10, 20, 30인 직원들만 새 테이블 emp123으로 생성
+DROP TABLE emp123;
+CREATE TABLE emp123 AS
+    (SELECT * FROM hr.employees 
+        WHERE department_id IN(10, 20, 30));
+        
+DESC emp123;
+SELECT first_name, salary, department_id FROM emp123;
+
+-- 부서가 30번인 직원들의 급여를 10 % 인상해보자
+UPDATE emp123
+SET salary = salary + salary * 0.1
+WHERE department_id = 30;
+
+SELECT * FROM emp123;
+
+
+-- DELETE
+-- 테이블로부터 특정 레코드를 삭제
+-- WHERE 절이 없으면 모든 레코드 삭제 (주의!!)
+
+--삭제: DELETE FROM 테이블명; ->전체 레코드 삭제(휴지통/복구가능)
+--삭제2: DELETE FROM 테이블명 WHERE 삭제할 레코드 조건;
+      --->삭제할 레코드 조건에 해당하는 레코드만 삭제
+--TRUNCATE TABLE 테이블명; ->전체 레코드 삭제 (속도빠름/완전삭제/복구불가)
+
+-- JOB_ID가 MK_로 시작하는 직원들 삭제
+DELETE FROM emp123 
+WHERE job_id LIKE 'MK_%';
+
+SELECT * FROM emp123;
+
+DELETE FROM emp123; -- WHERE 절이 생략된 DELETE문은 모든 레코드를 삭제 ->주의!
+SELECT * FROM emp123;
+
+ROLLBACK;
+
+---------------------------
+--TRANSACTION
+---------------------------
+-- 트랜젝션 테스트 테이블
+CREATE TABLE t_test(
+    log_text VARCHAR2(100)
+    );
+
+-- 첫 번쨰 DML이 수행된 시점에서 Transaction
+INSERT INTO t_test VALUES('트랜잭션 시작');
+SELECT * FROM t_test;
+INSERT INTO t_test VALUES ('데이터 INSERT');
+SELECT * FROM t_test;
+ 
+SAVEPOINT sp1;  -- 세이브 포인트 설정
+
+INSERT INTO t_test VALUES ('데이터 2 INSERT');
+SELECT * FROM t_test;
+
+SAVEPOINT sp2;  -- 세이브 포인트 설정
+
+UPDATE t_test SET log_text = '업데이트'; -- 모든 레코드가 '업데이트'로 바뀜(WHERE없음)
+SELECT * FROM t_test;
+
+ROLLBACK TO sp1;   --sp1로 귀환
+SELECT * FROM t_test;
+
+INSERT INTO t_test VALUES('데이터 3 INSERT');
+SELECT * FROM t_test;
+
+--반영: COMMIT / 취소 : ROLLBACK
+-- 명시적으로 Transaction 종료
+COMMIT;     --영구반영(cmd에서 확인 가능)
+SELECT * FROM t_test;
 
 
 
