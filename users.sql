5567d6e5b9cc7c91ff96ec4c86f67b9b90a872be
yeaeun123 <yeaeun0125@gmail.com>
2024-06-13 10:53:26 +0900

    myhome_users

---
+++ users.sql
@@ -0,0 +1,11 @@
+CREATE TABLE users (
+    no number primary key,
+    name varchar2(20) NOT NULL,
+    password varchar2(20) NOT NULL,
+    email varchar2(128) UNIQUE NOT NULL,
+    gender char(1) NOT NULL CHECK(gender IN('M', 'F')),
+    created_at date DEFAULT sysdate);
+    
+DESCRIBE users;
+
+CREATE SEQUENCE seq_users_pk;
