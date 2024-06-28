0a538d74b2aaabf6a0b38518c0f9dff64e6fa381
yeaeun123 <yeaeun0125@gmail.com>
2024-06-28 16:08:00 +0900

    phonebook sql추가

--- himedia_myportal.sql
+++ himedia_myportal.sql
@@ -1,3 +1,4 @@
+-- guestbook 테이블
 CREATE TABLE guestbook (
 	no number primary key,
     name varchar(20) NOT NULL,
@@ -17,9 +18,9 @@ commit;
 
 
 DROP TABLE users;
-DROP SEQUENCE seq_users_pk;
+DROP SEQUENCE seq_board_pk;
 
-
+--users 테이블
 CREATE TABLE users (
 	no number PRIMARY KEY,
     name varchar(20) NOT NULL,
@@ -34,4 +35,44 @@ CREATE SEQUENCE seq_users_pk
 
 DESCRIBE users;
 SELECT * FROM users;
+commit;
+
+--board 테이블]
+
+DROP TABLE board;
+DROP SEQUENCE seq_board_pk;
+
+CREATE TABLE board (
+	no number PRIMARY KEY,
+    title varchar2(128) NOT NULL,
+    content varchar2(255) NOT NULL,
+    hit number DEFAULT 0,
+    reg_date date DEFAULT sysdate,
+    user_no number NOT NULL
+);
+
+CREATE SEQUENCE seq_board_pk
+    START WITH 1
+    INCREMENT BY 1;
+
+    
+SELECT * FROM board;
+
+--사용자확인
+SELECT * FROM users;
+
+INSERT INTO board(no, title, content, user_no) 
+VALUES(seq_board_pk.nextval, '첫번째 게시물입니다.', '첫번째 게시물입니다. 잘 되나요?',
+13);
+
+		SELECT b.no, 
+				b.title,
+				b.content,
+				b.hit,
+				b.reg_date as regDate,
+				b.user_no as userNo,
+				u.name as userName 
+		FROM board b, users u 
+		WHERE b.user_no = u.no;
+        
 commit;
